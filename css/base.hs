a7bcb8989368941e60f7fe2148684676fd2a7c64
Linus Arver <linusarver@gmail.com>
2013-04-12 12:41:05 -0700

    change argument order for 'hv' and 'ev'
    Also use hv and ev where appropriate.

--- css/base.hs
+++ css/base.hs
@@ -21,15 +21,15 @@ rgbHex rgb'
 myStylesheet :: Css
 myStylesheet = do
 	html ? do
-		margin 0 0 0 0
-		padding 0 0 0 0
+		ev margin 0
+		ev padding 0
 		fontSize (pt 14)
 		backgroundColor (rgb 200 200 210)
 		color (grayish 30)
 		overflowY scroll
 	body ? do
 		width (px 750)
-		margin 0 auto 0 auto
+		hv margin 0 auto
 		sup ? do
 			"vertical-align" -: "top"
 			fontSize (em 0.6)
@@ -40,11 +40,11 @@ myStylesheet = do
 		fontWeight normal
 	div' ? do
 		"#header" & do
-			margin (em 0.5) 0 (em 0.5) 0
+			hv margin (em 0.5) 0
 			textAlign $ alignSide sideCenter
 		"#content" & do
-			padding (em 1) (em 2) (em 1) (em 2)
-			ev (px 3) borderRadius
+			hv padding (em 1) (em 2)
+			ev borderRadius (px 3)
 			backgroundColor (rgbHex 0xeeeeee)
 			boxShadow (px 0) (px 0) (px 3) (rgbHex 0x666666)
 			h1 ? do
@@ -62,26 +62,26 @@ myStylesheet = do
 		color (grayish 51)
 		backgroundColor $ grayish 248
 		border solid (px 1) (grayish 204)
-		ev (px 3) borderRadius
-		padding 0 (px 4) 0 (px 4)
+		ev borderRadius (px 3)
+		hv padding 0 (px 4)
 	pre ? do
 		code ? do
 			display block
 			overflow auto
-			padding (px 6) (px 10) (px 6) (px 10)
+			hv padding (px 6) (px 10)
 	a ? do
 		":hover" & do
 			textDecoration none
 
--- | A horizontal/vertical size helper function. It accepts a function and two
--- sizes for the horizontal and vertical parts. E.g., instead of calling
+-- | A horizontal/vertical size helper. It accepts a function and two sizes for
+-- the horizontal and vertical parts. E.g., instead of calling
 -- 		padding (px 6) (px 10) (px 6) (px 10)
 -- you can simply do
 --		hv padding (px 6) (px 10)
 -- to save some keystrokes and decrease the chance of typos.
-hv :: Size a -> Size a -> (Size a -> Size a -> Size a -> Size a -> Css) -> Css
-hv a b f = f a b a b
+hv :: (Size a -> Size a -> Size a -> Size a -> Css) -> Size a -> Size a -> Css
+hv f a b = f a b a b
 
 -- | Like "hv", but uses the same size for *everything*.
-ev :: Size a -> (Size a -> Size a -> Size a -> Size a -> Css) -> Css
-ev a f = f a a a a
+ev :: (Size a -> Size a -> Size a -> Size a -> Css) -> Size a -> Css
+ev f a = f a a a a
