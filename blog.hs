59b31011b86acd4aa453ce474c03a41564cebb9c
Linus Arver <linusarver@gmail.com>
2013-05-31 06:29:11 -0700

    add tags field
    Also do some misc stylistic changes.

--- blog.hs
+++ blog.hs
@@ -5,23 +5,41 @@ import Hakyll
 
 main :: IO ()
 main = hakyll $ do
-	{- match "images/*" $ do
-		route   idRoute
-		compile copyFileCompiler
-		-}
+	-- Build tags
+	tags <- buildTags "post/*" (fromCapture "tag/*.html")
 
+	tagsRules tags $ \tag pattern -> do
+		let
+			title = "posts tagged " ++ tag
+		route idRoute
+		compile $ do
+			list <- postList tags pattern recentFirst
+			makeItem ""
+				>>= loadAndApplyTemplate "template/archive.html"
+					(constField "title" title `mappend`
+						constField "body" list `mappend`
+						defaultContext)
+				>>= loadAndApplyTemplate "template/default.html"
+					(constField "title" title `mappend`
+						defaultContext)
+				>>= relativizeUrls
+
+	-- Add favicon
 	match "favicon.png" $ do
 		route idRoute
 		compile copyFileCompiler
 
+	-- Add raw CSS
 	match "css/*.css" $ do
 		route   idRoute
 		compile compressCssCompiler
 
+	-- Add Clay-based css
 	match "css/*.hs" $ do
 		route $ setExtension "css"
 		compile $ getResourceString >>= withItemBody (unixFilter "runghc" [])
 
+	-- Add some default pages
 	match (fromList ["about.md", "code.md"]) $ do
 		route   $ setExtension "html"
 		compile $ pandocCompiler
@@ -31,46 +49,62 @@ main = hakyll $ do
 	match "post/*" $ do
 		route $ setExtension "html"
 		compile $ pandocCompiler
-			>>= loadAndApplyTemplate "template/post.html"    postCtx
-			>>= loadAndApplyTemplate "template/default.html" postCtx
+			>>= loadAndApplyTemplate "template/post.html"    (tagsCtx tags)
+			>>= loadAndApplyTemplate "template/default.html" (tagsCtx tags)
 			>>= relativizeUrls
 
 	create ["archive.html"] $ do
 		route idRoute
 		compile $ do
-			let
-				archiveCtx =
-					field "post" (\_ -> postList recentFirst) `mappend`
-					constField "title" "Archive"              `mappend`
-					defaultContext
-
-			makeItem ""
+			posts <- loadAll "post/*"
+			sorted <- recentFirst posts
+			itemTpl <- loadBody "template/post-item.html"
+			list <- applyTemplateList itemTpl postCtx sorted
+			makeItem list
 				>>= loadAndApplyTemplate "template/archive.html" archiveCtx
 				>>= loadAndApplyTemplate "template/default.html" archiveCtx
 				>>= relativizeUrls
 
-
-	match "index.html" $ do
+	create ["index.html"] $ do
 		route idRoute
 		compile $ do
-			let
-				indexCtx = field "post" $ \_ -> postList $ fmap (take 3) . recentFirst
-
-			getResourceBody
-				>>= applyAsTemplate indexCtx
-				>>= loadAndApplyTemplate "template/default.html" postCtx
+			posts <- loadAll "post/*"
+			sorted <- fmap (take 3) $ recentFirst posts
+			itemTpl <- loadBody "template/post-item.html"
+			list <- applyTemplateList itemTpl postCtx sorted
+			makeItem list
+				>>= loadAndApplyTemplate "template/index.html" (homeCtx tags list)
+				>>= loadAndApplyTemplate "template/default.html" (homeCtx tags list)
 				>>= relativizeUrls
 
 	match "template/*" $ compile templateCompiler
 
 postCtx :: Context String
 postCtx =
-	dateField "date" "%B %e, %Y" `mappend`
+	dateField "date" "%Y-%m-%d" `mappend`
 	defaultContext
 
-postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
-postList sortFilter = do
-	posts <- sortFilter =<< loadAll "post/*"
+archiveCtx :: Context String
+archiveCtx =
+	constField "title" "Archive" `mappend`
+	defaultContext
+
+tagsCtx :: Tags -> Context String
+tagsCtx tags = tagsField "prettytags" tags `mappend` postCtx
+
+homeCtx :: Tags -> String -> Context String
+homeCtx tags list =
+	constField "post" list `mappend`
+	constField "title" "Home" `mappend`
+	field "taglist" (\_ -> renderTagList tags) `mappend`
+	defaultContext
+
+postList
+	:: Tags
+	-> Pattern
+	-> ([Item String] -> Compiler [Item String])
+	-> Compiler String
+postList tags pattern sortFilter = do
+	posts <- sortFilter =<< loadAll pattern
 	itemTpl <- loadBody "template/post-item.html"
-	list <- applyTemplateList itemTpl postCtx posts
-	return list
+	applyTemplateList itemTpl (tagsCtx tags) posts
