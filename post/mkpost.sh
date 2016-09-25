8bbbd539c27aca0eb40be6914eea1cf611259efb
Linus Arver <linusarver@gmail.com>
2016-09-24 21:54:24 -0700

    mkpost.sh: join and lowercase words
    Also add usage notes.

--- post/mkpost.sh
+++ post/mkpost.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env zsh
 
-date=$(date +%F)
-fname="$date-$@.org"
+setopt ERR_EXIT
+setopt NO_UNSET
+setopt PIPE_FAIL
+
+if (( $# == 0 )); then
+    >&2 echo "\
+Usage: mkpost.sh <WORD_1>...<WORD_N>
+    where WORD_1...WORD_N are word(s) separated by whitespace.
+
+Example:
+    mkpost.sh foo bar baz
+"
+    exit 1
+else
+fi
+
+sep='-'
+# `:gs/l/r' is a global substitution of `l' for `r'.
+joined=$(printf "${sep}%s" ${@:gs/%/%%})
+joined=${joined:${#sep}}
+
+fname="$(date +%F)-$(echo $joined | tr '[:upper:]' '[:lower:]').org"
 < template/default.org >> $fname
 emacs $fname & disown
