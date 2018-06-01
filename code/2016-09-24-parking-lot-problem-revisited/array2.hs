daaa91b445cfa900042f0aa664bce52b5a919b6e
Linus Arver <linusarver@gmail.com>
2018-06-01 07:55:28 -0700

    parking lot problem: fix typo
    We forgot to import array1.hs (included array2.hs twice).

--- code/2016-09-24-parking-lot-problem-revisited/array2.hs
+++ code/2016-09-24-parking-lot-problem-revisited/array2.hs
@@ -1,6 +1,12 @@
 import Data.Array (Array, elems)
 import Data.Array.ST (runSTArray, newArray, writeArray)
 
+minfreeArray2 :: [Int] -> Int
+minfreeArray2 = search . checklist
+
+search :: Array Int Bool -> Int
+search = length . takeWhile id . elems
+
 checklist :: [Int] -> Array Int Bool
 checklist xs = runSTArray $ do
 	a <- newArray (0, n) False
@@ -8,9 +14,3 @@ checklist xs = runSTArray $ do
 	return a
 	where
 	n = length xs
-
-search :: Array Int Bool -> Int
-search =  length . takeWhile id . elems
-
-minfreeArray2 :: [Int] -> Int
-minfreeArray2 = search . checklist
