f0dc353d6819269671951aa37d4f2ce5517e702a
Linus Arver <linusarver@gmail.com>
2021-02-15 09:34:26 -0800

    shell.nix: don't use "with" keyword
    See https://nix.dev/anti-patterns/language.html#with-attrset-expression.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,16 @@
 # https://stackoverflow.com/a/56180220/437583
 
-with import (builtins.fetchGit {
-    url = https://github.com/NixOS/nixpkgs;
-    ref = "master";
-    rev = "1998b95adc6b5a9aef336e45955a01575bd898f1";
-}) {};
+let
+    pkgs = import (builtins.fetchGit {
+        url = https://github.com/NixOS/nixpkgs;
+        ref = "master";
+        rev = "1998b95adc6b5a9aef336e45955a01575bd898f1";
+    }) {};
+in
 
-haskell.lib.buildStackProject {
+pkgs.haskell.lib.buildStackProject {
     name = "my-project";
     buildInputs = [
-      haskell.compiler.ghc8103
+      pkgs.haskell.compiler.ghc8103
       # For zlib (it is a transitive dependency).
-      zlib ]; }
+      pkgs.zlib ]; }
